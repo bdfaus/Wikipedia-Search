7e93b5d87355bf09d569a207baf43601b17667a1
BDFOZ <Ben.Faus@live.com>
2018-01-05 06:07:18 -0600

    message

--- wikipedia-s.js
+++ wikipedia-s.js
@@ -1,7 +1,14 @@
 $(document).keypress(function(e) {
+  //correctly assigns input to #search_box to var search after press of enter key
     if(e.which == 13) {
         var search = ($("#search_box").val());
     };
+    //attempting to test
+    $.get("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
+      function(data){
+        console.log(JSON.stringify(data));
+      }
+    );
     console.log(search);
   })
 
