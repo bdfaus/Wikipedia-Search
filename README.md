2828c098ed361c04c10e1af3f2d8682f5748ec25
BDFOZ <Ben.Faus@live.com>
2018-01-05 19:50:27 -0600

    Got the API working. Jquery adds elements according to number of search results.
    Last search result isn't bordered correctly, and there looks to be an empty div at the top of the results list. Probably correlated.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Wikipedia-Search
-A small web-app that returns Wikipedia search results
+A small web-app that returns Wikipedia search results. Completed as part of
+the Free Code Camp front-end certificate.
--- wikipedia-s.js
+++ wikipedia-s.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(document).on("keypress", function(e) {
+  $("body").on("keypress", function(e) {
     if(e.which == 13) {
       var search_text = ($("#search_box").val());
       $.ajax({
@@ -8,7 +8,14 @@ $(document).ready(function(){
         data: {action: "opensearch",search:search_text, format:"json", origin:"*"},
         success: function(data){
           console.log(JSON.stringify(data));
-          console.log(data[0].length);
+          for (x = 0; x < data[1].length; x++){
+            $(".results").append("<div class='result container'>");
+            $(".results").append("<h3 class= 'col-lg-3'>" + data[1][x] + "</h3>");
+            $(".results").append("<p class= 'col-lg-6'>" + data[2][x] + "</p>");
+            $(".results").append("<a class = 'col-lg-3' href = '" + data[3][x] + "'>" + "Read more" + "</a>");
+            $(".results").append("</div>");
+          }
+          console.log(data[1][3]);
         }
       });
     };
