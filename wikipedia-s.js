53b8ed620c02d98f18e73ad2ac26688a7cb9f854
BDFOZ <Ben.Faus@live.com>
2018-01-05 18:50:31 -0600

    moved API request inside keypress function. Still havening CORS difficulties.

--- wikipedia-s.js
+++ wikipedia-s.js
@@ -1,15 +1,17 @@
 $(document).keypress(function(e) {
   //correctly assigns input to #search_box to var search after press of enter key
     if(e.which == 13) {
-        var search = ($("#search_box").val());
+        var search_text = ($("#search_box").val());
+        $.ajax({
+          url: "https://en.wikipedia.org/w/api.php",
+          type: "GET",
+          data: {action: "opensearch",search:search_text, format:"jsonp"},
+          success: function(data){
+            console.log(JSON.stringify(data));
+          }
+        });
     };
-    //attempting to test
-    $.get("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
-      function(data){
-        console.log(JSON.stringify(data));
-      }
-    );
-    console.log(search);
+    console.log(search_text);
   })
 
 //use opensearch on wikipedia api
