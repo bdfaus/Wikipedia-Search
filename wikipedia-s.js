f33c4ebd76c21147f60dcb2c756f13b4772db6c3
BDFOZ <Ben.Faus@live.com>
2018-01-05 19:17:46 -0600

    successfully retrieiving from the Wikipedia API. Had to change data type to JSON, not JSONP Also had to set origin=*

--- wikipedia-s.js
+++ wikipedia-s.js
@@ -1,17 +1,19 @@
-$(document).keypress(function(e) {
-  //correctly assigns input to #search_box to var search after press of enter key
+$(document).ready(function(){
+  $(document).on("keypress", function(e) {
     if(e.which == 13) {
-        var search_text = ($("#search_box").val());
-        $.ajax({
-          url: "https://en.wikipedia.org/w/api.php",
-          type: "GET",
-          data: {action: "opensearch",search:search_text, format:"jsonp"},
-          success: function(data){
-            console.log(JSON.stringify(data));
-          }
-        });
+      var search_text = ($("#search_box").val());
+      $.ajax({
+        url: "https://en.wikipedia.org/w/api.php",
+        type: "GET",
+        data: {action: "opensearch",search:search_text, format:"json", origin:"*"},
+        success: function(data){
+          console.log(JSON.stringify(data));
+          console.log(data[0].length);
+        }
+      });
     };
     console.log(search_text);
   })
+})
 
 //use opensearch on wikipedia api
