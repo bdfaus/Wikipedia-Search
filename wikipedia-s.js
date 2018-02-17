5d05b0ca55e01831fa1cfb253ca66fcd4f72154f
BDFOZ <Ben.Faus@live.com>
2018-02-16 21:13:41 -0600

    Completed and then copied/pasted from codepen.io/bdfoz

--- wikipedia-s.js
+++ wikipedia-s.js
@@ -1,26 +1,27 @@
-$(document).ready(function(){
-  $("body").on("keypress", function(e) {
-    if(e.which == 13) {
-      var search_text = ($("#search_box").val());
-      $.ajax({
-        url: "https://en.wikipedia.org/w/api.php",
-        type: "GET",
-        data: {action: "opensearch",search:search_text, format:"json", origin:"*"},
-        success: function(data){
-          console.log(JSON.stringify(data));
-          for (x = 0; x < data[1].length; x++){
-            $(".results").append("<div class='result container'>");
-            $(".results").append("<h3 class= 'col-lg-3'>" + data[1][x] + "</h3>");
-            $(".results").append("<p class= 'col-lg-6'>" + data[2][x] + "</p>");
-            $(".results").append("<a class = 'col-lg-3' href = '" + data[3][x] + "'>" + "Read more" + "</a>");
-            $(".results").append("</div>");
-          }
-          console.log(data[1][3]);
-        }
-      });
-    };
-    console.log(search_text);
-  })
-})
 
-//use opensearch on wikipedia api
+$("body").on('keypress',function(e) {
+  if(e.which == 13) {
+    var search_text = $("#search_box").val();
+    $("input:text").val('');
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php",
+      type: "GET",
+      data: {action: "opensearch",search:search_text, format:"json", origin:"*"},
+      success: function(data){
+        console.log(JSON.stringify(data));
+        $('.results').empty();
+        for (x = 0; x < data[1].length; x++){
+          console.log(data[1][x]);
+          $(".results").append(`
+            <div class='result container'> 
+            <h1 class= 'result_head col-lg-3'>${data[1][x]}</h1>
+            <p class= 'col-lg-6'>${data[2][x]}</p>
+            <p><a class = 'col-lg-3' href = "${data[3][x]}"> Read more </a></p>
+            </div>`);
+        }
+        console.log(data[1][3]);
+      }
+    });
+  };
+  console.log(search_text);
+})
